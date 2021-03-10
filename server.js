4fb25c43590b6c7f5d702034dc4848724f76acc0
Rafael Fernandes Lopes <rafael.fernandes@gmail.com>
2021-03-09 21:25:30 -0300

    implementação da consulta do cardápio

--- server.js
+++ server.js
@@ -4,6 +4,31 @@ const app = express();
 
 const porta = process.env.PORT || 80;
 
+const bd = [
+    {
+        nome: "Calabresa",
+        preco: 15.0,
+    },
+    {
+        nome: "Carne de Sol",
+        preco: 20.0,
+    },
+    {
+        nome: "Frango com Catupiry",
+        preco: 10.0,
+    },
+    {
+        nome: "Portuguesa",
+        preco: 18.0,
+    },
+];
+
+app.use(express.json());
+
+app.get("/pizza/cardapio", (req, res) => {
+    res.send(bd);
+});
+
 app.get("/", (req, res) => {
     res.send("Olá mundo!");
 });
